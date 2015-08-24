e361a94558b5c7bc6a7110b1a53656adfeb13674
DanielMaida <dfm2@cin.ufpe.br>
2015-08-24 16:58:00 -0300

    Repository cleanup

--- Project/src/main/java/com/software/project/dao/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.software.project.dao;
-
-import com.software.project.entities.User;
-
-public interface UserDAO extends GenericDAO<User, Long>{
-	User login(String username, String password);
-}
